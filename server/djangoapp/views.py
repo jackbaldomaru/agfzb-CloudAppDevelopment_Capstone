89643c5eff124d3b324739ea2049428dfc49c1e2
jackbaldomaru <this.is.my.work.shit@gmail.com>
2023-06-03 16:41:46 -0400

    proxy ibm fn PROBLM 1

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .models import CarDealer
+from .restapis import get_dealers_from_cf, get_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -107,9 +107,18 @@ def registration_request(request):
             return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+#def get_dealerships(request):
+#    context = {}
+#    if request.method == "GET":
+#        return render(request, 'djangoapp/index.html', context)
 def get_dealerships(request):
+    print("dealerships")
     context = {}
     if request.method == "GET":
+        url = "https://eu-de.functions.appdomain.cloud/api/v1/web/ddae85a9-b5d3-434a-b801-bd8e746894d1/dealership-package/get-dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
 
